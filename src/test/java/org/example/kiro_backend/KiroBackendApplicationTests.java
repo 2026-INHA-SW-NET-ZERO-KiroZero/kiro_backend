acbce87471fb6626218e57f325e8d37c2a44695b
eclipse021 <yahang2008@naver.com>
2026-06-26 19:17:35 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/org/example/kiro_backend/KiroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kiro_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
