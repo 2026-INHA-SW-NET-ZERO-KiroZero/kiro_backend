ba9f6ec9720808c4584be299ce5c996ddca95cea
chazy-d <my0710my@gmail.com>
2026-06-27 01:44:08 +0900

    chore: configure netzero runtime foundation

--- src/main/java/com/kirozero/netzero/KiroBackendApplication.java
+++ src/main/java/com/kirozero/netzero/KiroBackendApplication.java
@@ -1,4 +1,4 @@
-package org.example.kiro_backend;
+package com.kirozero.netzero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class KiroBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(KiroBackendApplication.class, args);
     }
-
 }
--- src/test/java/com/kirozero/netzero/KiroBackendApplicationTests.java
+++ src/test/java/com/kirozero/netzero/KiroBackendApplicationTests.java
@@ -1,13 +1,14 @@
-package org.example.kiro_backend;
+package com.kirozero.netzero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class KiroBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
